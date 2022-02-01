1a7419b14476e37380c30f9f339adca7a0337326
adrianlsq2000 <adrianlautaro2000@gmail.com>
2022-01-31 20:39:00 -0500

    Actualizando

--- ProyectoRover/src/main/java/ec/edu/espol/proyectorover/App.java
+++ ProyectoRover/src/main/java/ec/edu/espol/proyectorover/App.java
@@ -7,19 +7,33 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.application.Platform;
+import vista.VistaExplorar;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
+    public static String ruta="src\\main\\java\\recursos/";
+    public static final double DISTANCE_X = 20;
+    public static final double DISTANCE_Y = 20;
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+      scene = new Scene(loadFXML("Principal"), 640, 480);
+      // scene= new Scene(new VistaExplorar().getRaiz(), 1350, 675);
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(e->{
+           // Optional<ButtonType> result = Alerta.confirmation();
+            if(true){          
+                Platform.exit();
+            }else{
+                e.consume();
+            }});
+
+      
     }
 
     static void setRoot(String fxml) throws IOException {
