48f0ff6b7bc8b11d5d928ecdbf2e40b27c7341fe
aargguev <aargguev@espol.edu.ec>
2022-01-31 22:56:19 -0500

    FXML

--- ProyectoRover/src/main/java/module-info.java
+++ ProyectoRover/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ec.edu.espol.proyectorover {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens ec.edu.espol.proyectorover to javafx.fxml;
     exports ec.edu.espol.proyectorover;
